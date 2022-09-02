54d5f7e24193eab1e699b037b559e9e049552de1
KevinKaeLee <76722891+CoolbreezeKevin@users.noreply.github.com>
2022-09-02 16:22:05 +0800

    Update fair1M_label_to_fair1M_5.py

--- fair1M_label_to_fair1M_5.py
+++ fair1M_label_to_fair1M_5.py
@@ -1,17 +1,5 @@
+## Put it into PATH/JDet/python/jdet/devkits
 import os
-# from jdet.config.constant import FAIR1M_1_5_CLASSES
-# FAIR1M_1_5_CLASSES = ['Airplane', 'Ship', 'Vehicle', 'Basketball_Court', 'Tennis_Court', 
-#         "Football_Field", "Baseball_Field", 'Intersection', 'Roundabout', 'Bridge'
-#     ]
-
-# FAIR_CLASSES_ = ['Boeing737', 'Boeing747', 'Boeing777', 'Boeing787', 'C919', 
-#         'A220', 'A321', 'A330', 'A350', 'ARJ21', 'other-airplane', 'Passenger_Ship', 
-#         'Motorboat', 'Fishing_Boat', 'Tugboat', 'Engineering_Ship', 'Liquid_Cargo_Ship', 
-#         'Dry_Cargo_Ship', 'Warship', 'other-ship', 'Small_Car', 'Bus', 'Cargo_Truck', 
-#         'Dump_Truck', 'Van', 'Trailer', 'Tractor', 'Excavator', 'Truck_Tractor', 
-#         'other-vehicle', 'Basketball_Court', 'Tennis_Court', 'Football_Field', 
-#         'Baseball_Field', 'Intersection', 'Roundabout', 'Bridge'
-#     ]
 
 NeedChange_Airplane = {'Boeing737', 'Boeing747', 'Boeing777', 'Boeing787', 'C919', 
         'A220', 'A321', 'A330', 'A350', 'ARJ21', 'other-airplane', }
